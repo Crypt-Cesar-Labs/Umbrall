6aa5cbef106ce6ec95d148655b629507e1aee452
Cesar <cesar.arthur.mtz.hdz@gmail.com>
2022-02-22 09:06:23 -0600

    Se hace la interfaz para testeo de comunicación TCP, para cliente y servidor. Se configura el socket del Servidor. Todo: Empezar con el socket del cliente.

--- Umbrall/tcpTest.cs
+++ Umbrall/tcpTest.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +18,69 @@ namespace Umbrall
         public tcpTest()
         {
             InitializeComponent();
+        }
+
+        public class NetworkServer
+        {
+            public void Start()
+            {
+                //Creamos un TcpListener y le indicamos que puerto
+                //va a poner en escucha
+                TcpListener tcpListener = new TcpListener(9898);
+
+                //Iniciamos la escucha
+                tcpListener.Start();
+
+                //este metodo queda blouqeado hasta que se conecte un cliente
+                Socket socketForClient = tcpListener.AcceptSocket();
+
+                if(socketForClient.Connected)
+                {
+                    //Si se conecta
+                    Console.WriteLine("Cliente conectado.");
+
+                    //Creamos el networkStream, el reader y el writing
+                    NetworkStream networkStream = new NetworkStream(socketForClient);
+                    StreamWriter streamWriter = new StreamWriter(networkStream);
+                    StreamReader streamReader = new StreamReader(networkStream);
+
+                    //Esta es la data a enviar
+                    string theString = "Esta es la data de envio";
+
+                    try
+                    {
+                        //Escribamos la data en el stream
+                        streamWriter.WriteLine(theString);
+
+                        //Ahora le decimos que la mande
+                        streamWriter.Flush();
+
+                        //Esperamos data del cliente
+                        //y la escribimos por consola
+                        theString = streamReader.ReadLine();
+                        Console.WriteLine(theString);
+                    }
+                    finally
+                    {
+                        //Cerramos las conexiones
+                        streamReader.Close();
+                        streamWriter.Close();
+                        networkStream.Close();
+                        socketForClient.Close();
+                    }
+                }
+            }
+        }
+
+        private void btnServerTcpConnect_Click(object sender, EventArgs e)
+        {
+            
+            new Thread(new ThreadStart( delegate { new NetworkServer().Start();})).Start();
+        }
+
+        private void btnClientTcpConnect_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
