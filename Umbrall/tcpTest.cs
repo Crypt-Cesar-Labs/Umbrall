c17e5743f05421449e029ee21aaf75680d1b21ef
Cesar <cesar.arthur.mtz.hdz@gmail.com>
2022-02-22 10:42:42 -0600

    Se creo el socket para la conexion del cliente. Conexion del cliente con el modulo RS-485 - WiFi. La configuracion es en el código.
    To-do:  1. Hay que hacer que la configuración sea legible desde los textbox para el servidor y el puerto.  2. Leer los datos recibidos desde el modulo WiFi.

--- Umbrall/tcpTest.cs
+++ Umbrall/tcpTest.cs
@@ -72,15 +72,65 @@ namespace Umbrall
             }
         }
 
+
+        //Se crea el evento del boton para la conexion del servidor
         private void btnServerTcpConnect_Click(object sender, EventArgs e)
         {
             
             new Thread(new ThreadStart( delegate { new NetworkServer().Start();})).Start();
         }
 
+        public class ClientBase
+        {
+            internal class Connect
+            {
+                public Connect()
+                {
+                    //MessageBox.Show("Metodo Connect en Client");
+                    TcpClient socketForServer;
+                    string server = "10.10.100.254";
+
+                    try
+                    {
+                        //Creamos un TcpCliente y le pasamos 
+                        //el server del puerto
+                        socketForServer = new TcpClient(server, 8899);
+                    }
+                    catch
+                    {
+                        Console.WriteLine("No se pudo conectar a {0}:9898", server);
+                        return;
+                    }
+
+                    //aqui es lo mismo que en el server. Usamos StreamWriter y Reader.
+                    NetworkStream networkStream = socketForServer.GetStream();
+
+                    StreamReader streamReader = new System.IO.StreamReader(networkStream);
+
+                    StreamWriter streamWriter = new System.IO.StreamWriter(networkStream);
+
+                    try
+                    {
+                        string outputString = streamReader.ReadLine();
+                        Console.WriteLine(outputString);
+                        //streamWriter.WriteLine("Mensaje desde el cliente");
+                        //streamWriter.Flush();
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Exception reading from server");
+                    }
+                    finally
+                    {
+                        //networkStream.Close();
+                    } 
+                }
+            }
+        }
+        //Se crea el evento del boton para la conexion del cliente
         private void btnClientTcpConnect_Click(object sender, EventArgs e)
         {
-
+            new ClientBase.Connect();
         }
     }
 }
