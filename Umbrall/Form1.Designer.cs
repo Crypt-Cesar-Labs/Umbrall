3ccfda0913e4d17f01cdce659e16c14b7ff19eaa
cesar <cesar.arthur.mtz.hdz@gmail.com>
2022-02-25 09:30:40 -0600

    Cambios de prueba, nuevo elemento en el boton de Test de la pestaña de herramientas, para el cliente TCP con socket para usar el metdodo Send.  To-do: implementar el cliente con sockets para usar el metodo Send.

--- Umbrall/Form1.Designer.cs
+++ Umbrall/Form1.Designer.cs
@@ -48,6 +48,7 @@
             this.modbusRTUOverTCPTestToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.modbusTCPTestToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.clientTCPTestToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clientSocketTCPTestToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -62,7 +63,8 @@
             this.testToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1924, 30);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(1203, 24);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -75,7 +77,7 @@
             this.configuraciónToolStripMenuItem,
             this.salirToolStripMenuItem});
             this.menúToolStripMenuItem.Name = "menúToolStripMenuItem";
-            this.menúToolStripMenuItem.Size = new System.Drawing.Size(60, 26);
+            this.menúToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
             this.menúToolStripMenuItem.Text = "Menú";
             // 
             // iniciarMonitoreoToolStripMenuItem
@@ -83,7 +85,7 @@
             this.iniciarMonitoreoToolStripMenuItem.Image = global::Umbrall.Properties.Resources.play;
             this.iniciarMonitoreoToolStripMenuItem.Name = "iniciarMonitoreoToolStripMenuItem";
             this.iniciarMonitoreoToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.I)));
-            this.iniciarMonitoreoToolStripMenuItem.Size = new System.Drawing.Size(272, 26);
+            this.iniciarMonitoreoToolStripMenuItem.Size = new System.Drawing.Size(216, 22);
             this.iniciarMonitoreoToolStripMenuItem.Text = "Iniciar Monitoreo";
             // 
             // detenerMonitoreoToolStripMenuItem
@@ -91,13 +93,13 @@
             this.detenerMonitoreoToolStripMenuItem.Image = global::Umbrall.Properties.Resources.stop;
             this.detenerMonitoreoToolStripMenuItem.Name = "detenerMonitoreoToolStripMenuItem";
             this.detenerMonitoreoToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.D)));
-            this.detenerMonitoreoToolStripMenuItem.Size = new System.Drawing.Size(272, 26);
+            this.detenerMonitoreoToolStripMenuItem.Size = new System.Drawing.Size(216, 22);
             this.detenerMonitoreoToolStripMenuItem.Text = "Detener Monitoreo";
             // 
             // toolStripSeparator1
             // 
             this.toolStripSeparator1.Name = "toolStripSeparator1";
-            this.toolStripSeparator1.Size = new System.Drawing.Size(269, 6);
+            this.toolStripSeparator1.Size = new System.Drawing.Size(213, 6);
             // 
             // configuraciónToolStripMenuItem
             // 
@@ -106,53 +108,53 @@
             this.tarifasCFEToolStripMenuItem,
             this.alertasCorreoToolStripMenuItem});
             this.configuraciónToolStripMenuItem.Name = "configuraciónToolStripMenuItem";
-            this.configuraciónToolStripMenuItem.Size = new System.Drawing.Size(272, 26);
+            this.configuraciónToolStripMenuItem.Size = new System.Drawing.Size(216, 22);
             this.configuraciónToolStripMenuItem.Text = "Configuración";
             // 
             // conectarDispositivoToolStripMenuItem
             // 
             this.conectarDispositivoToolStripMenuItem.Name = "conectarDispositivoToolStripMenuItem";
-            this.conectarDispositivoToolStripMenuItem.Size = new System.Drawing.Size(229, 26);
+            this.conectarDispositivoToolStripMenuItem.Size = new System.Drawing.Size(183, 22);
             this.conectarDispositivoToolStripMenuItem.Text = "Conectar Dispositivo";
             // 
             // tarifasCFEToolStripMenuItem
             // 
             this.tarifasCFEToolStripMenuItem.Name = "tarifasCFEToolStripMenuItem";
-            this.tarifasCFEToolStripMenuItem.Size = new System.Drawing.Size(229, 26);
+            this.tarifasCFEToolStripMenuItem.Size = new System.Drawing.Size(183, 22);
             this.tarifasCFEToolStripMenuItem.Text = "Tarifas CFE";
             this.tarifasCFEToolStripMenuItem.Click += new System.EventHandler(this.tarifasCFEToolStripMenuItem_Click);
             // 
             // alertasCorreoToolStripMenuItem
             // 
             this.alertasCorreoToolStripMenuItem.Name = "alertasCorreoToolStripMenuItem";
-            this.alertasCorreoToolStripMenuItem.Size = new System.Drawing.Size(229, 26);
+            this.alertasCorreoToolStripMenuItem.Size = new System.Drawing.Size(183, 22);
             this.alertasCorreoToolStripMenuItem.Text = "Alertas Correo";
             // 
             // salirToolStripMenuItem
             // 
             this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
             this.salirToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.X)));
-            this.salirToolStripMenuItem.Size = new System.Drawing.Size(272, 26);
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(216, 22);
             this.salirToolStripMenuItem.Text = "Salir";
             this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
             // 
             // reportesToolStripMenuItem
             // 
             this.reportesToolStripMenuItem.Name = "reportesToolStripMenuItem";
-            this.reportesToolStripMenuItem.Size = new System.Drawing.Size(82, 26);
+            this.reportesToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
             this.reportesToolStripMenuItem.Text = "Reportes";
             // 
             // acercaDeToolStripMenuItem
             // 
             this.acercaDeToolStripMenuItem.Name = "acercaDeToolStripMenuItem";
-            this.acercaDeToolStripMenuItem.Size = new System.Drawing.Size(89, 26);
+            this.acercaDeToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
             this.acercaDeToolStripMenuItem.Text = "Acerca de";
             this.acercaDeToolStripMenuItem.Click += new System.EventHandler(this.acercaDeToolStripMenuItem_Click);
             // 
             // ayudaToolStripMenuItem
             // 
             this.ayudaToolStripMenuItem.Name = "ayudaToolStripMenuItem";
-            this.ayudaToolStripMenuItem.Size = new System.Drawing.Size(65, 26);
+            this.ayudaToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
             this.ayudaToolStripMenuItem.Text = "Ayuda";
             // 
             // testToolStripMenuItem
@@ -162,55 +164,63 @@
             this.modbusRTUTestToolStripMenuItem,
             this.modbusRTUOverTCPTestToolStripMenuItem,
             this.modbusTCPTestToolStripMenuItem,
-            this.clientTCPTestToolStripMenuItem});
+            this.clientTCPTestToolStripMenuItem,
+            this.clientSocketTCPTestToolStripMenuItem});
             this.testToolStripMenuItem.Name = "testToolStripMenuItem";
-            this.testToolStripMenuItem.Size = new System.Drawing.Size(49, 26);
+            this.testToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
             this.testToolStripMenuItem.Text = "Test";
             // 
             // tCPIPTestToolStripMenuItem
             // 
             this.tCPIPTestToolStripMenuItem.Name = "tCPIPTestToolStripMenuItem";
-            this.tCPIPTestToolStripMenuItem.Size = new System.Drawing.Size(267, 26);
+            this.tCPIPTestToolStripMenuItem.Size = new System.Drawing.Size(213, 22);
             this.tCPIPTestToolStripMenuItem.Text = "TCP/IP Test";
             this.tCPIPTestToolStripMenuItem.Click += new System.EventHandler(this.tCPIPTestToolStripMenuItem_Click);
             // 
             // modbusRTUTestToolStripMenuItem
             // 
             this.modbusRTUTestToolStripMenuItem.Name = "modbusRTUTestToolStripMenuItem";
-            this.modbusRTUTestToolStripMenuItem.Size = new System.Drawing.Size(267, 26);
+            this.modbusRTUTestToolStripMenuItem.Size = new System.Drawing.Size(213, 22);
             this.modbusRTUTestToolStripMenuItem.Text = "Modbus RTU Test";
             this.modbusRTUTestToolStripMenuItem.Click += new System.EventHandler(this.modbusRTUTestToolStripMenuItem_Click);
             // 
             // modbusRTUOverTCPTestToolStripMenuItem
             // 
             this.modbusRTUOverTCPTestToolStripMenuItem.Name = "modbusRTUOverTCPTestToolStripMenuItem";
-            this.modbusRTUOverTCPTestToolStripMenuItem.Size = new System.Drawing.Size(267, 26);
+            this.modbusRTUOverTCPTestToolStripMenuItem.Size = new System.Drawing.Size(213, 22);
             this.modbusRTUOverTCPTestToolStripMenuItem.Text = "Modbus RTU over TCP Test";
             this.modbusRTUOverTCPTestToolStripMenuItem.Click += new System.EventHandler(this.modbusRTUOverTCPTestToolStripMenuItem_Click);
             // 
             // modbusTCPTestToolStripMenuItem
             // 
             this.modbusTCPTestToolStripMenuItem.Name = "modbusTCPTestToolStripMenuItem";
-            this.modbusTCPTestToolStripMenuItem.Size = new System.Drawing.Size(267, 26);
+            this.modbusTCPTestToolStripMenuItem.Size = new System.Drawing.Size(213, 22);
             this.modbusTCPTestToolStripMenuItem.Text = "Modbus TCP Test";
             this.modbusTCPTestToolStripMenuItem.Click += new System.EventHandler(this.modbusTCPTestToolStripMenuItem_Click);
             // 
             // clientTCPTestToolStripMenuItem
             // 
             this.clientTCPTestToolStripMenuItem.Name = "clientTCPTestToolStripMenuItem";
-            this.clientTCPTestToolStripMenuItem.Size = new System.Drawing.Size(267, 26);
+            this.clientTCPTestToolStripMenuItem.Size = new System.Drawing.Size(213, 22);
             this.clientTCPTestToolStripMenuItem.Text = "Client TCP Test";
             this.clientTCPTestToolStripMenuItem.Click += new System.EventHandler(this.clientTCPTestToolStripMenuItem_Click);
             // 
+            // clientSocketTCPTestToolStripMenuItem
+            // 
+            this.clientSocketTCPTestToolStripMenuItem.Name = "clientSocketTCPTestToolStripMenuItem";
+            this.clientSocketTCPTestToolStripMenuItem.Size = new System.Drawing.Size(213, 22);
+            this.clientSocketTCPTestToolStripMenuItem.Text = "Client Socket TCP Test";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1924, 1055);
+            this.ClientSize = new System.Drawing.Size(1203, 716);
             this.Controls.Add(this.menuStrip1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.IsMdiContainer = true;
             this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Umbrall - Monitor de Energía";
             this.menuStrip1.ResumeLayout(false);
@@ -241,6 +251,7 @@
         private System.Windows.Forms.ToolStripMenuItem modbusRTUOverTCPTestToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem modbusTCPTestToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem clientTCPTestToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clientSocketTCPTestToolStripMenuItem;
     }
 }
 
